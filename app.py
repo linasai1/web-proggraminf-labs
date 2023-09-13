c7ca0daa5810450de451981d771c5a9d761a3d58
Алина Сайфулина <alisai.saifulina@gmail.com>
2023-09-13 18:24:49 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,57 +1,64 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
+    return redirect("/menu", code=302)
+
+@app.route("/menu")
+def menu():
     return """
-<!doctype html>
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-           НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных.
-        </header>
+    <!doctype html>
+    <html>
+        <head>
+            <title>НГТУ, ФБ, Лабораторные работы</title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных.
+            </header>
 
-        <main>
-           <ol>
-               <li>
-                   <a href="/lab1" target="_blank">Лабораторная работа 1</a>
-               </li>
-            </ol>
-        </main>
+            <main>
+                <ol>
+                    <li>
+                        <a href="/lab1" target="_blank">Лабораторная работа 1</a>
+                    </li>
+                </ol>
+            </main>
 
-        <footer>
-            &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-"""
+            <footer>
+                &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
+            </footer>
+        </body>
+    </html>
+    """
+
 @app.route("/lab1")
 def lab1():
     return """
-<!doctype html>
-<html>
-    <head>
-        <title>Сайфулина Алина Витальевна, Лабораторная 1</title>
-    </head>
-    <body>
-        <header>
-           НГТУ, ФБ, Лабораторная работа 1
-        </header>
+    <!doctype html>
+    <html>
+        <head>
+            <title>Сайфулина Алина Витальевна, Лабораторная 1</title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, Лабораторная работа 1
+            </header>
 
-        <div>
-           Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, 
-           а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
-           сознательно предоставляющих лишь самые базовые возможности.
-        </div>
+            <main>
+                <div>
+                    Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, 
+                    а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+                    сознательно предоставляющих лишь самые базовые возможности.
+                </div>
+            </main>
 
-        <footer>
-            &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-"""
+            <footer>
+                &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
+            </footer>
+        </body>
+    </html>
+    """
 
