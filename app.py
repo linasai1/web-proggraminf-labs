dc81a7713b8df3ec4e21203835028c76a1515755
Алина Сайфулина <alisai.saifulina@gmail.com>
2023-09-13 18:01:12 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,35 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+           НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных.
+        </header>
+
+        <main>
+           <ol>
+               <li>
+                   <a href="/lab1" target="_blank">Лабораторная работа 1</a>
+               </li>
+            </ol>
+        </main>
+
+        <footer>
+            &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -14,7 +42,11 @@ def start():
            НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h1>
+        <div>
+           Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, 
+           а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+           сознательно предоставляющих лишь самые базовые возможности.
+        </div>
 
         <footer>
             &copy; Сайфулина Алина, ФБИ-13, 3 курс, 2023
@@ -22,3 +54,4 @@ def start():
     </body>
 </html>
 """
+
