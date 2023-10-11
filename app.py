60738b19fcfa5f3ec1ff9792d9e880ecef191895
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-10-11 17:04:34 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -128,3 +128,6 @@ def EQ ():
         </body>
     </html>
     '''
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
