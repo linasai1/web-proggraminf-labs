601a20648336b57db5f2c136cf7ba05a60e2acf0
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-11-22 16:51:23 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -4,3 +4,35 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+         return render_template('login.html')   
+    error = ''   
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    if not username:        
+            error = 'Введите логин'
+            return render_template('login.html', error=error, username=username, password=password)
+    else: 
+        if username != 'alex' and password != '123':
+            error = 'Неверный логин и/или пароль' 
+            return render_template('login.html', error=error, username=username, password=password)
+        elif username != 'alex' and password == '123':
+            error = 'Неверный логин'
+            return render_template('login.html', error=error, username=username, password=password)
+        elif password != '123' and username == 'alex':
+            error = 'Неверный пароль'
+            return render_template('login.html', error=error, username=username, password=password)
+        elif not password:
+            error = 'Введите пароль'
+            return render_template('login.html', error=error, username=username, password=password)
+        elif not username and not password:
+            error = 'Введите пароль и логин'
+            return render_template('login.html', error=error, username=username, password=password)
+        else:
+            return render_template ('password_success.html', error=error, 
+                                        username=username, password=password)    
+    return render_template('login.html', error=error)
