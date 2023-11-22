58fd96ac6e54750df08dee95edf3331700b36046
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-11-22 16:57:45 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -36,3 +36,30 @@ def login():
             return render_template ('password_success.html', error=error, 
                                         username=username, password=password)    
     return render_template('login.html', error=error)
+
+
+@lab4.route('/lab4/password_success/')
+def success():
+    return render_template('/lab4/password_success')    
+    
+
+@lab4.route('/lab4/refrigerator/', methods=['GET', 'POST'])
+def refrigerator():
+    message = ''
+    temperature = request.form.get('temperature')
+    if not temperature:
+        message = 'Ошибка: не задана температура'
+    else:
+            temperature = int(temperature)
+            if temperature < -12:
+                message = 'Не удалось установить температуру — слишком низкое значение'
+            elif -12 <= temperature <= -9:
+                message = f'Установлена температура: {temperature}°С\n❄❄❄'
+            elif -8 <= temperature <= -5:
+                message = f'Установлена температура: {temperature}°С\n❄❄'
+            elif -4 <= temperature <= -1:
+                message = f'Установлена температура: {temperature}°С\n❄'
+            elif temperature > -1:
+                message = 'Не удалось установить температуру — слишком высокое значение'
+            return render_template('temperature.html', message=message)
+    return render_template('refrigerator.html')
