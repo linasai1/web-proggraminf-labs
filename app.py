faaa0130aaba74f18bb1771ce1e9d45d1e25ed73
Alina Saifulina <alisai.saifulina@gmail.com>
2024-01-15 22:15:19 +0700

    Правки

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ from lab6 import lab6
 
 app = Flask(__name__)
 app.secret_key = '123'
+user_db = 'Alina_Saifulina_knowledge_base_orm'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'knowlebge_base_orm'
+password = '123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+
+login_manager.login_view = 'lab6.login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_users(user_id):
+    return users.query.get(int(user_id))
 
 
 app = Flask(__name__)
